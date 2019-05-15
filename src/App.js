199159b754446a5da6a521315c4f4d18a7fd7160
sammieeblaq <odianoseairehrour@gmail.com>
2019-05-15 13:25:58 +0100

    creating class based components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './App.css'
-import Product from "./components/Product"
-import productData from "./vschoolProduct"
 
-const App = () => {
-	const productComponent = productData.map(item => <Product product={ item } />)
-	
-	return (
-		<div>
-			{ productComponent }
-		</div>
-	)
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Code goes here</h1>
+			</div>
+		)
+	}
 }
 
 export default App;
