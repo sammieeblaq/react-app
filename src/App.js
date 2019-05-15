ee524a47e16602cf6ce13301f7d6ee7af24dd8af
sammieeblaq <odianoseairehrour@gmail.com>
2019-05-16 00:08:03 +0100

    Event handling in react

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react'
 
+function handleClick() {
+    console.log("I was clicked")
+}
+
 class App extends Component {
     state = {
-        isLoggedIn: true
+
     }
     render() {
-        let wordDisplay
-        if (this.state.isLoggedIn) {
-            wordDisplay = "in"
-        } else {
-            wordDisplay = "out"
-        }
         return (
             <div>
-                <h1>You are currently logged { wordDisplay }</h1>
+                <p>Hello World</p>
+                <br/>
+                <button onClick={ handleClick }>Click Me</button>
             </div>
         )
     }
