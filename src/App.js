dee5f52de218937db2c70ad19e51434afe7d2c3c
sammieeblaq <odianoseairehrour@gmail.com>
2019-05-15 23:38:30 +0100

    solved problem with react

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react'
-import "./App.css"
 
 class App extends Component {
-	state = {
-		name: "Sally",
-		age: 34
-	}
-
-	render() {
-		return (
-			<div>
-				<h1>{ this.state.name }</h1>
-				<h3>{ this.state.age } years old!</h3>
-			</div>
-		)
-	}
+    state = {
+        isLoggedIn: true
+    }
+    render() {
+        let wordDisplay
+        if (this.state.isLoggedIn) {
+            wordDisplay = "in"
+        } else {
+            wordDisplay = "out"
+        }
+        return (
+            <div>
+                <h1>You are currently logged { wordDisplay }</h1>
+            </div>
+        )
+    }
 }
 
-export default App
+export default App;
