d48d10045b3784d9b4e567bb2bd3799dfbbe7532
sammieeblaq <odianoseairehrour@gmail.com>
2019-06-06 21:23:50 +0100

    added checkbox and radio button

--- src/App.js
+++ src/App.js
@@ -4,19 +4,20 @@ import "./App.css";
 class App extends Component {
     state = {
         firstName: "",
-        lastName: ""
+        lastName: "",
+        isFriendly: true,
+        gender: "",
+        favColor: ""
     }
 
     handleChange = (e) => {
-        const { name, value } = e.target;
-        this.setState({
-            [name]: value
-        })
+        const { name, value, type, checked } = e.target;
+        type === "checkbox" ? this.setState({ [name]: checked }) : this.setState({ [name]: value })
     }
 
     render() {
         return (
-            <form action="">
+            <form>
                 <input 
                     value={ this.state.firstName }
                     name="firstName"
@@ -30,6 +31,57 @@ class App extends Component {
                     placeholder="Last Name" 
                     onChange={ this.handleChange } 
                 />
+                
+                <br/>
+                <textarea
+                    value={"some default value"}
+                    onChange={ this.handleChange }
+                />
+                <br/>
+                <label>
+                    Is Friendly
+                    <input
+                        type="checkbox"
+                        name="isFriendly"
+                        checked={ this.state.isFriendly }
+                        onChange={ this.handleChange }
+                    />
+                </label>
+                <br/>
+                <label>
+                    Male
+                    <input
+                        type="radio"
+                        name="gender"
+                        value="male"
+                        checked={ this.state.gender === "male" }
+                        onChange={ this.handleChange }
+                    />
+                </label>
+                <br/>
+                <label>
+                    Female
+                    <input
+                        type="radio"
+                        name="gender"
+                        value="female"
+                        checked={ this.state.gender === "female" }
+                        onChange={ this.handleChange }              
+                    />
+                </label>
+                <br/>
+                <select 
+                    value={ this.state.favColor }
+                    onChange={ this.handleChange }
+                    name="favColor"
+                >
+                    <option value="blue">Blue</option>
+                    <option value="green">Green</option>
+                    <option value="yellow">Yellow</option>
+                    <option value="red">Red</option>
+                    <option value="orange">Orange</option>
+                </select>
+                <button>Submit</button>
             </form>
         )
     }
